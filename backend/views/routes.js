d9ac6deedf7694e2aeefc33b8ffaad0981dfdc70
Tan Ye Kai <tykcuber@gmail.com>
2021-12-13 12:52:12 +0800

    finished login and register bcrypt

--- backend/views/routes.js
+++ backend/views/routes.js
@@ -1,5 +1,6 @@
 // Import controlers
 const staffController = require('../controllers/staffController');
+const authController= require('../controllers/authController');
 const checkUserFn = require('../middlewares/checkUser');
 
 // Match URL's with controllers
@@ -10,10 +11,12 @@ exports.appRoute = router => {
         router.post('/api/user/process-submission', checkUserFn.getClientUserId, userController.processDesignSubmission);
         router.put('/api/user/',checkUserFn.getClientUserId,checkUserFn.checkAdmin, userController.processUpdateOneUser);
         router.put('/api/user/design/',checkUserFn.getClientUserId, userController.processUpdateOneDesign);
-    
+     
         router.get('/api/user/process-search-design/:pagenumber/:search?', checkUserFn.getClientUserId, userController.processGetSubmissionData);
         router.get('/api/user/process-search-user/:pagenumber/:search?', checkUserFn.getClientUserId,checkUserFn.checkAdmin, userController.processGetUserData);
         router.get('/api/user/:recordId', checkUserFn.getClientUserId,checkUserFn.checkAdmin, userController.processGetOneUserData); */
         router.get('/test', staffController.processGetAllStaff);
+        router.post('/login', authController.processLogin);
+        router.post('/register', authController.processRegister);
 
 };
