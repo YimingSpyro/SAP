801e9c83c60d5091771fde1a744745c3772775c3
Tan Yong Jian <64192594+Pakuluku@users.noreply.github.com>
2021-12-22 14:41:24 +0800

    Backend APIs

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+const config = require('./config');
+//To find out more on createPool:
+//https://www.npmjs.com/package/mysql#pooling-connections
+/* const pool = mysql.createPool({
+        connectionLimit: 100,
+        host: 'remotemysql.com',
+        user: 'Htge9DZLeE',
+        password: 'J9px4se9RA',
+        database: 'Htge9DZLeE',
+        multipleStatements: true
+    }); */
+ const pool = mysql.createPool({
+        connectionLimit: 100,
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'tas',
+        multipleStatements: true
+    });
+
+ module.exports=pool;
