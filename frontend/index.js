a391c654f88f9ddf125f9d3b2eef559a5e374a16
Tan Yong Jian <64192594+Pakuluku@users.noreply.github.com>
2021-12-28 17:17:49 +0800

    File Structure

--- frontend/index.js
+++ frontend/index.js
@@ -5,13 +5,13 @@ const cookieparser = require("cookie-parser");
 app.use(express.static('public'))
 app.use(cookieparser());
 const myPort = 8000
-app.get("/login", (req, res) => {
+/* app.get("/login", (req, res) => {
     res.sendFile("/public/login.html", { root: __dirname });
 });
 
 app.get("/test", (req, res) => {
     res.sendFile("/public/home.html", { root: __dirname });
-});
+}); */
   
 app.listen(myPort,() => {
     console.log(`Client Server started and accessible via port ${myPort}`);
