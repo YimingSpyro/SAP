41c6015e762b957e0611bda2460162a0970ca821
Zhi Lin <49969188+ZhiLinR@users.noreply.github.com>
2022-01-22 00:06:16 +0800

    completed assignment report

--- backend/services/downloadsService.js
+++ backend/services/downloadsService.js
@@ -3,8 +3,8 @@ const pool = require('../config/database')
 
 module.exports.getAssignmentReport = (acad_sem) => {
     return new Promise((resolve, reject) => {
-        pool.query('SELECT SUM(ma_lecture), SUM(ma_tutorial), SUM(ma_practical), mod_code, mod_stage, mod_abbrv, lecture_class, practical_class, tutorial_class, fk_course_id FROM mod_assign INNER JOIN  module ON mod_assign.fk_mod_code = module.mod_code WHERE mod_assign.fk_semester_code = ? GROUP BY mod_code, fk_course_id;',
-            [acad_sem],(err, rows) => {
+        pool.query('SELECT SUM(ma_lecture), SUM(ma_tutorial), SUM(ma_practical), mod_code, mod_stage, mod_abbrv, lecture_class, practical_class, tutorial_class, fk_course_id FROM mod_assign INNER JOIN  module ON mod_assign.fk_mod_code = module.mod_code WHERE mod_assign.fk_semester_code = ? AND module.fk_semester_code = ? GROUP BY mod_code;',
+            [acad_sem,acad_sem],(err, rows) => {
                 if (err) {
                     reject(err);
                 } else {
--- frontend/public/script/reports.js
+++ frontend/public/script/reports.js
@@ -1,27 +1,95 @@
+
 function ExportToExcel(type, fn, dl) {
+    //using the npm package SheetJS, we will export the whole html table
     var table_data = document.getElementById('admin-table');
     var ws_name = "Assignment Report";
     var ws = XLSX.utils.table_to_sheet(table_data);
 
     var wscols = [
-        {wch:20},
-        {wch:10},
-        {wch:10},
-        {wch:10}
+        { wch: 20 },
+        { wch: 10 },
+        { wch: 10 },
+        { wch: 10 }
     ];
     ws['!cols'] = wscols;
+    var style = {
+        right: {
+            style: "thin",
+            color: "000000"
+        },
+        left: {
+            style: "thin",
+            color: "000000"
+        },
+        top: {
+            style: "thin",
+            color: "000000"
+        },
+        bottom: {
+            style: "thin",
+            color: "000000"
+        },
+    }
+    for (const i in ws) {
+        if (typeof (ws[i]) != "object") continue;
+        let cell = XLSX.utils.decode_cell(i);
+        if (cell.r != 0) { ws[i].s = { border: style } }; //style all the columns except first
+        if (cell.c != 0 && cell.r != 0) { //style the ltp value columns
+            ws[i].s = {
+                border: style,
+                alignment: {
+                    horizontal: "center",
+                    vertical: "center"
+                }
+            }
+        }
+        if (cell.r % 5 == 1) { // every elective row
+            ws[i].s = {
+                fill: { // background color
+                    patternType: "solid",
+                    fgColor: { rgb: "ff9933" },
+                    bgColor: { rgb: "ff9933" }
+                },
+                font: {
+                    bold: true
+                }
+            }
 
+        } else if (cell.r % 5 == 2) { //every class,ltp row
+            ws[i].s = {
+                border: style,
+                font: {
+                    bold: true
+                }
+            }
+        }
+        if (cell.c != 0 && cell.r % 5 == 2) { //class,ltp header row
+            ws[i].s = {
+                border: style,
+                alignment: {
+                    horizontal: "center",
+                    vertical: "center"
+                },
+                font: {
+                    bold: true
+                }
+            }
+        }
+        ws["A1"].s = {
+            font: {
+                sz: 13.5,
+                bold: true
+            }
+        }
+
+    }
     var wb = XLSX.utils.book_new();
-    
+
     XLSX.utils.book_append_sheet(wb, ws, ws_name);
     return dl ?
-        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
-        XLSX.writeFile(wb, fn || (ws_name+'.' + (type || 'xlsx')));
+        XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64', showGridLines: false }) :
+        XLSX.writeFile(wb, fn || (ws_name + '.' + (type || 'xlsx')));
 }
-
-/* function downloadReport(file_id, filename) {
-    window.open(`${base_url}/reports/download/${file_id}/${filename}`, '_blank')
-}; */
 //get available semesters
 function getSemesters() {
     axios.get(base_url + '/api/semester/').then((results) => {
@@ -40,7 +108,7 @@ function getTAReports(acad_sem) {
         results.forEach(element => {
             let table_row = `
             <tr>
-            <th colspan= "4">${element['mod_code']} ${element['mod_abbrv']} : ${element['mod_abbrv']} ${element['fk_course_id']} ${element['mod_stage']} ${acad_sem}</th>
+            <th colspan= "4">${element['mod_code']} ${element['mod_abbrv']} : ${element['mod_abbrv']} ${element['fk_course_id']} ${element['mod_stage']} </th>
             </tr>
             <tr>
             <th>Classes</th>
@@ -59,9 +127,13 @@ function getTAReports(acad_sem) {
             <td>${element['SUM(ma_lecture)']}</td>
             <td>${element['SUM(ma_tutorial)']}</td>
             <td>${element['SUM(ma_practical)']}</td>
+            </tr>
+            <tr>
+            <td colspan ="4"></td>
             </tr>`
             $('#ta-report-table').append(table_row)
         });
+        document.getElementById("acad-year-head").innerHTML = acad_sem;
         $('#admin-table').append(`<caption id= 'caption'>Showing ${response.data.length} Results</caption>`)
     }).catch((error) => {
         //window.alert(error.response.data.message)
