15dac2a7fdae282b6176dc3acd61864725f42b99
Tan Yong Jian <64192594+Pakuluku@users.noreply.github.com>
2022-02-04 00:19:32 +0800

    Announcement Integration, Bug Fixes

--- frontend/public/script/global.js
+++ frontend/public/script/global.js
@@ -3,11 +3,65 @@ function getSemester() {
         .then(response => response.data[0])
         .catch(err => error(err));
 };
-async function setCurrentSemester(){
+function getNavAnnouncement() {
+    return axios.get(base_url + '/api/announcements?order=ASC')
+        .then(response => response.data)
+        .catch(err => error(err));
+}
+async function setCurrentSemester() {
     let semester = await getSemester();
     let semester_code = semester.semester_code
-    sessionStorage.setItem('semester_code',semester_code);
+    sessionStorage.setItem('semester_code', semester_code);
 }
+
+async function generateNavAnnouncements() {
+    $("#notification-bell").empty();
+    let announcements = await getNavAnnouncement();
+    let roles = JSON.parse(sessionStorage.getItem("staff_role"));
+    let assigned_role = false;
+    for (let index = 0; index < announcements.length; index++) {
+        const announcement = announcements[index];
+        let announcement_roles = JSON.parse(announcement.fk_announcement_roles)
+        let announcement_start = new Date(announcement.announcement_start.slice(0, 10) + "T24:00:00")
+        let announcement_end = new Date(announcement.announcement_end.slice(0, 10) + "T24:00:00")
+        let current_date = new Date()
+        if (current_date >= announcement_start && current_date <= announcement_end) {
+            for (let index = 0; index < roles.length; index++) {
+                const role = roles[index];
+                if (announcement_roles.includes(parseInt(role)) || parseInt(role) == 1) {
+                    assigned_role = true;
+                }
+            }
+            if (assigned_role) {
+                $("#notification-bell").append(`
+                <div class="card card-body `+ announcement.announcement_type.toLowerCase() + `" id="announcementcontent">
+                <a href="/announcements">
+                    <div id="announcementtitle">
+                    `+ announcement.announcement_subject + `
+                    </div>
+                    <div id="announcementmessage">
+                    `+ announcement.announcement_message + `
+                    </div>
+                </a>
+                </div>`)
+            }
+        }
+    }
+    if (!assigned_role) {
+        $("#notification-bell").append(`
+        <div class="card card-body general" id="announcementcontent">
+        <a href="/announcements">
+            <div id="announcementtitle">
+                No New Announcements
+            </div>
+            <div id="announcementmessage">
+                Check Back Later
+            </div>
+        </a>
+        </div>`)
+    }
+}
+
 let base_url = "http://localhost:8080";
 
 
@@ -20,5 +74,10 @@ function getProfilePicture() {
         }
     });
 }
-setCurrentSemester()
-getProfilePicture()
+
+$(document).ready(() => {
+    setCurrentSemester()
+    getProfilePicture()
+    generateNavAnnouncements();
+})
+
