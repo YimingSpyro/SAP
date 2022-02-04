84cec3ad261f7b34e1c85fe8d49ef76716299850
Tan Yong Jian <64192594+Pakuluku@users.noreply.github.com>
2022-02-05 01:02:19 +0800

    Scrunge Cat has been hunted down and purged, changed default profile picture, input validation

--- frontend/public/script/profile.js
+++ frontend/public/script/profile.js
@@ -1,30 +1,21 @@
 const staff_id = sessionStorage.getItem("staff_id");
-function getProfileInfo(){
-    return axios.get(base_url + '/api/staff/'+staff_id+'')
-    .then(response => response.data)
-    .catch(err => error(err));
+function getProfileInfo() {
+    return axios.get(base_url + '/api/staff/' + staff_id + '')
+        .then(response => response.data)
+        .catch(err => error(err));
 };
 
-function updateProfileInfo(){
-    let config = {
-        headers: {
-            'Content-Type': 'multipart/form-data'
-        }
-    }
-    axios.put(base_url + '/api/staff/personal/'+staff_id+'',
-    {
-        staff_name: $("#staff-name")[0].value,
-        staff_abbrv: $("#staff-abbrv")[0].value,
-        staff_email: $("#staff-email")[0].value,
-        staff_number: $("#staff-contactnumber")[0].value,
-        staff_mobile: $("#staff-mobilenumber")[0].value,
-        staff_remarks: $("#staff-remarks")[0].value
-    })
-    /* .then(() => {
-        return axios.post(base_url + '/uploads/profile-picture/8405',{},config)
-    }) */
-    .then(() => success())
-    .catch(err => error(err));
+function updateProfileInfo() {
+    axios.put(base_url + '/api/staff/personal/' + staff_id + '',
+        {
+            staff_abbrv: $("#staff-abbrv")[0].value,
+            staff_email: $("#staff-email")[0].value,
+            staff_number: $("#staff-contactnumber")[0].value,
+            staff_mobile: $("#staff-mobilenumber")[0].value,
+            staff_remarks: $("#staff-remarks")[0].value
+        })
+        .then(() => success())
+        .catch(err => error(err));
 }
 
 async function generateProfile() {
@@ -41,12 +32,38 @@ async function generateProfile() {
     $("#staff-remarks")[0].value = data.staff_remarks;
 }
 
+async function inputValidation() {
+    let upper = new RegExp('^[A-Z]+$')
+    let numbers = new RegExp('^[0-9]+$')
+    let remarks = new RegExp('^[a-zA-Z1-9,!.? ]+$')
+    let staff_abbrv = $("#staff-abbrv")[0].value
+    let staff_number =$("#staff-contactnumber")[0].value
+    let staff_mobile = $("#staff-mobilenumber")[0].value
+    let staff_remarks = $("#staff-remarks")[0].value
+    if (!numbers.test(staff_number) || !numbers.test(staff_mobile)) {
+        throw "Only numbers are allowed for contact info."
+    }
+    if (!remarks.test(staff_remarks)) {
+        throw "Only alphanumerics and ' ,  !  .  ? ' allowed for remarks."
+    }
+    if (!upper.test(staff_abbrv)) {
+        throw "Only uppercase letters allowed for staff abbrv."
+    }
+
+
+
+}
+
 
 // START OF SCRIPT
 generateProfile();
 
 $(document).ready(() => {
     $("#update-profile").click(() => {
-        updateProfileInfo();
+        inputValidation()
+        .then(()=>{
+            updateProfileInfo();
+        })
+        .catch(err => error(err))
     })
 })
