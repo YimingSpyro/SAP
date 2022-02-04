b954a4104f65c821afae58ab8e6cc27b849c27a9
Tan Ye Kai <tykcuber@gmail.com>
2022-02-05 01:49:42 +0800

    change databased details to env

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,6 +2,7 @@
 const dotenv = require('dotenv');
 dotenv.config(); //Build the process.env object.
 module.exports = {
+    databaseHost: process.env.DB_HOST,
     databaseUserName: process.env.DB_USERNAME,
     databasePassword: process.env.DB_PASSWORD,
     databaseName: process.env.DB_DATABASE_NAME,
