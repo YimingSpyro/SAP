3264bfc455905a5980ed8bac5ce305f4abf4ed82
Tan Yong Jian <64192594+Pakuluku@users.noreply.github.com>
2022-02-08 22:09:06 +0800

    Revert "Merge branch 'master' into hosting-branch"
    This reverts commit 3c05599177613d81009cdf4c98acf15799210673, reversing
    changes made to 5dd2f6084a5ae5b510b856e957e3ef9cc7c392b3.

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,9 @@ let app = express();
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    //res.setHeader('Access-Control-Allow-Origin', 'http://soctas2021.irc.sg');
+    res.setHeader('Access-Control-Allow-Origin', 'https://soctas2021.irc.sg:8000');
+    //res.setHeader('Access-Control-Allow-Origin', 'https://localhost:8000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
