c55a38291f8c49b2f44f02c79c4bd61e829f076e
Tan Yong Jian <64192594+Pakuluku@users.noreply.github.com>
2022-02-09 23:27:46 +0800

    Revert "Revert "Revert "Revert "Developer Mode""""
    This reverts commit 66cc8a79c59d21851a2e4d39c93627a1a01a53be.

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,10 @@ let app = express();
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
     //Localhost Dev URL
-    //res.setHeader('Access-Control-Allow-Origin', 'https://localhost:8000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://localhost:8000');
     
     //Live URL
-    res.setHeader('Access-Control-Allow-Origin', 'https://soctas2021.irc.sg:8000');
+    //res.setHeader('Access-Control-Allow-Origin', 'https://soctas2021.irc.sg:8000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
