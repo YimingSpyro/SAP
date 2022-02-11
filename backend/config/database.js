598fe87c0a35c0c871cec0ffe09b0d1d744280b4
Tan Ye Kai <tykcuber@gmail.com>
2022-02-11 17:25:29 +0800

    switched to devlopment branch

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const config = require('./config');
 //To find out more on createPool:
 //https://www.npmjs.com/package/mysql#pooling-connections
-/* //Localhost Dev URL
+//Localhost Dev URL
 const pool = mysql.createPool({
         connectionLimit: 100,
         host: 'sap-united-db.chaoomqaxpcs.ap-southeast-1.rds.amazonaws.com',
@@ -10,14 +10,14 @@ const pool = mysql.createPool({
         password: 'SbFdYtnnFqOjdi2ra5NK',
         database: 'tas',
         multipleStatements: true
-}); */
+});
 //Live URL
-const pool = mysql.createPool({
+/* const pool = mysql.createPool({
         connectionLimit: 65,
         host: config.databaseHost,
         user: config.databaseUserName,
         password: config.databasePassword,
         database: config.databaseName,
         multipleStatements: true
-});
+}); */
  module.exports=pool;
