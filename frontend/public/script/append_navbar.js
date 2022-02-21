6905cbdab5efbac6900e30dc92e448d349d7c355
SP-SOC-TAS-2021 <sp.soc.tas.2021@gmail.com>
2022-02-21 16:06:51 +0800

    Update append_navbar.js

--- frontend/public/script/append_navbar.js
+++ frontend/public/script/append_navbar.js
@@ -77,6 +77,43 @@ async function appendDashboard() {
     $('#dashboard-view-inner-wrap div.row').append(sessionStorage.getItem("dashboardContent"))
   }
 }
+async function generateDashboardItems() {
+  await appendDashboard()
+    .then(async () => {
+      if (location.pathname == "/home") {
+
+        // DASHBOARD ITEM CONTENT CODE GOES HERE
+        let data = await getAssignedModulesDashboard();
+        let total_hours = 0;
+        for (let index = 0; index < data.length; index++) {
+          const module = data[index];
+          let hours = (module.ma_lecture * module.mod_lecture + module.ma_tutorial * module.mod_tutorial + module.ma_practical * module.mod_practical) / 15;
+          total_hours += hours;
+          $(".assigned-modules").append(`
+        <tr>
+            <td id="module-`+ index + `">` + module.mod_code + ` ` + module.fk_course_id + `: ` + module.mod_name + ` (` + module.mod_abbrv + `) Year ` + module.mod_stage + `</th>
+            <td>`+ module.ma_lecture + `</td>
+            <td>`+ module.ma_tutorial + `</td>
+            <td>`+ module.ma_practical + `</td>
+            <td>`+ hours.toFixed(1) + `</td>
+        </tr>`);
+          if (module.fk_mod_coord == sessionStorage.getItem('staff_id')) {
+            $("#module-" + index).css("color", "orange");
+          }
+        }
+        $(".total-hours").append(`
+        <tr>
+            <th>Total Hours: `+ total_hours + `</th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </tr>`);
+      }
+
+
+    })
+}
 function getAssignedModulesDashboard() {
   return axios.get(base_url + '/api/module/assign/' + sessionStorage.getItem('staff_id') + '?semester_code=' + sessionStorage.getItem('semester_code'))
     .then(response => response.data)
@@ -87,110 +124,8 @@ $(document).ready(() => {
     var staff_name = sessionStorage.getItem('staff_name')
     $('#staff-name-text').text(staff_name);
   })
-  
-  appendDashboard()  
-    .then(async () => {
-      if (location.pathname=="/home") {
-        let staff_role = JSON.parse(sessionStorage.getItem('staff_role'))
-        // DASHBOARD ITEM CONTENT CODE GOES HERE
-        if (staff_role.includes('2')) {
-          $('.dashboard-item-lecturer').append(`
-          <table class="table table-dark ">
-          <thead>
-              <tr>
-                  <th scope="col">Classes</th>
-                  <th scope="col">L</th>
-                  <th scope="col">T</th>
-                  <th scope="col">P</th>
-                  <th scope="col">Hours</th>
-              </tr>
-          </thead>
-          <tbody class="assigned-modules">
-  
-          </tbody>
-          <thead class="total-hours">
-  
-          </thead>
-        </table>
-        `)
-          let data = await getAssignedModulesDashboard();
-          let total_hours = 0;
-          for (let index = 0; index < data.length; index++) {
-            const module = data[index];
-            let hours = (module.ma_lecture * module.mod_lecture + module.ma_tutorial * module.mod_tutorial + module.ma_practical * module.mod_practical) / 15;
-            total_hours += hours;
-            $(".assigned-modules").append(`
-          <tr>
-              <td id="module-`+ index + `">` + module.mod_code + ` ` + module.fk_course_id + `: ` + module.mod_name + ` (` + module.mod_abbrv + `) Year ` + module.mod_stage + `</th>
-              <td>`+ module.ma_lecture + `</td>
-              <td>`+ module.ma_tutorial + `</td>
-              <td>`+ module.ma_practical + `</td>
-              <td>`+ hours.toFixed(1) + `</td>
-          </tr>`);
-            if (module.fk_mod_coord == sessionStorage.getItem('staff_id')) {
-              $("#module-" + index).css("color", "orange");
-            }
-          }
-          $(".total-hours").append(`
-          <tr>
-              <th>Total Hours: `+ total_hours + `</th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
-          </tr>`);
-        }
-        if (staff_role.includes('4')) {
-          $('.dashboard-item-mc').append(`
-          <table class="table table-dark ">
-          <thead>
-              <tr>
-                  <th scope="col">Classes</th>
-                  <th scope="col">L</th>
-                  <th scope="col">T</th>
-                  <th scope="col">P</th>
-                  <th scope="col">Hours</th>
-              </tr>
-          </thead>
-          <tbody class="assigned-modules">
-  
-          </tbody>
-          <thead class="total-hours">
-  
-          </thead>
-        </table>
-        `)
-          let data = await getAssignedModulesDashboard();
-          let total_hours = 0;
-          for (let index = 0; index < data.length; index++) {
-            const module = data[index];
-            let hours = (module.ma_lecture * module.mod_lecture + module.ma_tutorial * module.mod_tutorial + module.ma_practical * module.mod_practical) / 15;
-            total_hours += hours;
-            $(".assigned-modules").append(`
-          <tr>
-              <td id="module-`+ index + `">` + module.mod_code + ` ` + module.fk_course_id + `: ` + module.mod_name + ` (` + module.mod_abbrv + `) Year ` + module.mod_stage + `</th>
-              <td>`+ module.ma_lecture + `</td>
-              <td>`+ module.ma_tutorial + `</td>
-              <td>`+ module.ma_practical + `</td>
-              <td>`+ hours.toFixed(1) + `</td>
-          </tr>`);
-            if (module.fk_mod_coord == sessionStorage.getItem('staff_id')) {
-              $("#module-" + index).css("color", "orange");
-            }
-          }
-          $(".total-hours").append(`
-          <tr>
-              <th>Total Hours: `+ total_hours + `</th>
-              <th></th>
-              <th></th>
-              <th></th>
-              <th></th>
-          </tr>`);
-        }
-      }
+  generateDashboardItems();
 
-
-    })
 
 })
 
