9c610867f4fb11896f658d961d69223daa0fdf0f
Zhi Lin <49969188+ZhiLinR@users.noreply.github.com>
2022-04-20 14:54:52 +0800

    Update staffhoursmaintenance.js

--- frontend/public/script/staffhoursmaintenance.js
+++ frontend/public/script/staffhoursmaintenance.js
@@ -45,7 +45,7 @@ function updateStaffType(){
 }
 
 async function inputValidation(type) {
-    let description_check = new RegExp('[\w||\s]')
+    let description_check = new RegExp('^\w+[-,"\'\s]+[\w\-,"\'\s]*')
     let type_check = new RegExp('^[A-Za-z0-9 ]+$')
     let hours = new RegExp('^[0-9]+$')
     if (type == "create") {
@@ -59,10 +59,10 @@ async function inputValidation(type) {
         if (!type_check.test(create_type) ) {
             throw "Only alphanumerics allowed for staff type."
         }
-        if (description_check.test(create_description)) {
+        if (!description_check.test(create_description)) {
             throw "Only alphanumeric characters and spaces allowed for description."
         }
-        if (description_check.test(create_remarks)) {
+        if (!description_check.test(create_remarks)) {
             throw "Only alphanumeric characters and spaces allowed for remarks."
         }
     }
@@ -77,10 +77,10 @@ async function inputValidation(type) {
         if (!type_check.test(update_type) ) {
             throw "Only numeric inputs allowed for hours."
         }
-        if (description_check.test(update_description)) {
+        if (!description_check.test(update_description)) {
             throw "Only alphanumeric characters and spaces allowed for description."
         }
-        if (description_check.test(update_remarks)) {
+        if (!description_check.test(update_remarks)) {
             throw "Only alphanumeric characters and spaces allowed for remarks."
         }
     }
